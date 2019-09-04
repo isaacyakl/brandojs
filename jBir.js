8dc817c3ba6a85fd9422167143e29bc36425510e
Isaac "Yak" L <i@isaacyakl.com>
2019-09-04 15:39:02 -0700

    Fix various function bugs

--- jBir.js
+++ jBir.js
@@ -4,7 +4,7 @@
 // jBir object
 // useful in initializing and controlling all image rotators
 const jBir = {
-  version: "0.0.4", // Version number string
+  version: "0.0.5", // Version number string
   jBirImgRotators: [], // Image rotators array
   jBirPreloadedImgs: [], // Images that have already been preload into the document
 
@@ -23,7 +23,8 @@ const jBir = {
     return false; // Return false image is not yet preloaded
   },
   // Function for initializing a new image rotator for a specific set of selected elements
-  // returns new image rotator object or undefined if selector returns no elements
+  // returns new image rotator object or error code if selector returns no elements
+  // return codes: 1 - no elements found with selector string
   new: function(
     selector_string, // element selector string
     image_url_arr, // array of image urls
@@ -35,25 +36,26 @@ const jBir = {
   ) {
     // create new image rotator for element(s)
     let newjBirImgRotator = new jBirImgRotator();
-    if (selector_string != undefined)
-      newjBirImgRotator.selector_string = selector_string; // if a selector string was provided store it
+    if (selector_string != undefined) newjBirImgRotator.selector_string = selector_string; // if a selector string was provided store it
     // generate element(s) list
-    newjBirImgRotator.elements = document.querySelectorAll(
-      newjBirImgRotator.selector_string
-    );
+    newjBirImgRotator.elements = document.querySelectorAll(newjBirImgRotator.selector_string);
     // if elements list is empty
-    if ((newjBirImgRotator.elements.length = 0)) return; // selector string returned no elements; abort image rotator creation
-    if (image_url_arr != undefined)
-      newjBirImgRotator.image_url_arr = image_url_arr; // if an image array was was provided store it
+    if (newjBirImgRotator.elements.length == 0) return 1; // selector string returned no elements; abort image rotator creation
+    if (image_url_arr != undefined) newjBirImgRotator.image_url_arr = image_url_arr; // if an image array was was provided store it
     if (interval_ms != undefined) newjBirImgRotator.interval_ms = interval_ms; // if an interval was specified store it
-    if (transition_ms != undefined)
-      newjBirImgRotator.transition_ms = transition_ms; // if a transition time was given store it
-    if (transition_type != undefined)
-      newjBirImgRotator.transition_type = transition_type; // if a transition type was chosen store it
-    if (random_order != undefined)
-      newjBirImgRotator.random_order = random_order; // if random order was set store it
+    if (transition_ms != undefined) newjBirImgRotator.transition_ms = transition_ms; // if a transition time was given store it
+    if (transition_type != undefined) newjBirImgRotator.transition_type = transition_type; // if a transition type was chosen store it
+    if (random_order != undefined) newjBirImgRotator.random_order = random_order; // if random order was set store it
     if (background_attachment != undefined)
       newjBirImgRotator.background_attachment = background_attachment; // if background attachment property was given store it
+
+    // Store original transition property
+    newjBirImgRotator.og_transition_property = newjBirImgRotator.elements[0].style.transition;
+
+    // Store original background properties
+    newjBirImgRotator.og_background_size_property =
+      newjBirImgRotator.elements[0].style.backgroundSize;
+    newjBirImgRotator.og_background_property = newjBirImgRotator.elements[0].style.background;
 
     // Preload images
     // For each image url in the array
@@ -61,7 +63,7 @@ const jBir = {
       // If the image has not yet been preloaded in the preload array
       if (!this.isPreloadedImg(image_url)) {
         let newImg = document.createElement("img"); // Create new img element
-        newImg.style.display = "none"; // Set it not to display
+        newImg.style.display = "none"; // Just incase, set it not to display even though it is never added to document
 
         // When an image cannot be loaded
         newImg.onerror = function() {
@@ -85,27 +87,22 @@ const jBir = {
 
     // Check if selector already has an image rotator handling it
     // Iterate through image rotators array
-    for (let i = 0; i < this.jBirImgRotators.length; i++) {
+    this.jBirImgRotators.forEach(jBirImgRotator => {
       // If existing rotator has same selector string as new rotator
-      if (
-        this.jBirImgRotators[i].selector_string ==
-        newjBirImgRotator.selector_string
-      ) {
-        this.pause(this.jBirImgRotators[i]); // pause rotator before deleting it to avoid duplicate img rotators
-        this.jBirImgRotators.splice(i, 1); // delete existing image rotator
-        continue; // leave loop since existing rotator was already found
+      if (jBirImgRotator.selector_string === newjBirImgRotator.selector_string) {
+        // Store original transition property
+        newjBirImgRotator.og_transition_property = jBirImgRotator.og_transition_property;
+
+        // Store original background properties
+        newjBirImgRotator.og_background_size_property = jBirImgRotator.og_background_size_property;
+        newjBirImgRotator.og_background_property = jBirImgRotator.og_background_property;
+
+        this.remove(jBirImgRotator); // remove rotator to avoid duplicate img rotators
+        return; // stop forEach since existing rotator was already handled
       }
-    }
+    });
 
-    // Store original transition property
-    newjBirImgRotator.og_transition_property =
-      newjBirImgRotator.elements[0].style.transition;
-
-    // Store original background properties
-    newjBirImgRotator.og_background_size_property =
-      newjBirImgRotator.elements[0].style.backgroundSize;
-    newjBirImgRotator.og_background_property =
-      newjBirImgRotator.elements[0].style.background;
+    this.jBirImgRotators.push(newjBirImgRotator); // add new image rotator object to array of rotators
 
     // Set transition properties on the element(s)
     newjBirImgRotator.elements.forEach(element => {
@@ -116,7 +113,6 @@ const jBir = {
         newjBirImgRotator.transition_type;
     });
 
-    this.jBirImgRotators.push(newjBirImgRotator); // add new image rotator object to array of rotators
     this.next(newjBirImgRotator); // move to first image
     this.play(newjBirImgRotator); // start image rotator
 
@@ -127,7 +123,6 @@ const jBir = {
   next: function(jBirImgRotator) {
     // if already transitioning to next image per previous call
     if (jBirImgRotator.transitioning) return 1; // return error code 1 without changing image to prevent spamming which glitches the transition animation
-
     jBirImgRotator.transitioning = true; // set transitioning variable to true because we are in process of changing images
     let indexOfNextImg = jBirImgRotator.last_image; // defaults to last image url
 
@@ -137,9 +132,7 @@ const jBir = {
       jBirImgRotator.image_url_arr.includes(this.jBirPreloadedImgs[0].src) // The preloaded image is one of this rotator's
     ) {
       // Set the index of the next image to be the one preloaded image
-      indexOfNextImg = jBirImgRotator.image_url_arr.indexOf(
-        this.jBirPreloadedImgs[0].src
-      );
+      indexOfNextImg = jBirImgRotator.image_url_arr.indexOf(this.jBirPreloadedImgs[0].src);
     } else {
       // Otherwise we have more preload options available
       // if randomize is on
@@ -148,20 +141,18 @@ const jBir = {
         do {
           // Choose a random index for next img between 0 and the image url array length minus 1
           indexOfNextImg = Math.floor(
-            Math.random() * (jBirImgRotator.image_url_arr.length - 1 - 0 + 1) +
-              0
+            Math.random() * (jBirImgRotator.image_url_arr.length - 1 - 0 + 1) + 0
           ); // Math.floor(Math.random() * (max - min + 1) + min);
-        } while ( // while there are more than 1 image urls to choose from and the already set image is not chosen again
+        } while (
           indexOfNextImg == jBirImgRotator.last_image &&
           jBirImgRotator.image_url_arr.length != 1
-        );
+        ); // while there are more than 1 image urls to choose from and the already set image is not chosen again
       }
       // else randomize is off
       else {
         // Move to next image url index or start over the image url index
         indexOfNextImg =
-          jBirImgRotator.last_image + 1 >
-          jBirImgRotator.image_url_arr.length - 1 // if we have reached the last image url
+          jBirImgRotator.last_image + 1 > jBirImgRotator.image_url_arr.length - 1 // if we have reached the last image url
             ? (indexOfNextImg = 0) // set index to first image url
             : (indexOfNextImg = ++jBirImgRotator.last_image); // otherwise set index to the next img (or if we have never set one yet set to the first [0] from [-1] the default last_img value)
       }
@@ -172,6 +163,7 @@ const jBir = {
         return 2; // return error code 2 without changing image because it is not yet preloaded
       }
     }
+
     // If we got this far
 
     // Set new background image(s)
@@ -219,9 +211,11 @@ const jBir = {
     return 0; // success
   },
   // Function to start a rotator
+  // returns 0 for success and 1 for already playing
   play: function(jBirImgRotator) {
-    // to prevent spamming rotators
-    if (jBirImgRotator.rotator_id != null) jBir.pause(jBirImgRotator); // if a rotator id is set, pause that one first
+    // To prevent spamming rotators
+    // If rotator id already exists then image rotator is already playing
+    if (jBirImgRotator.rotator_id != null) return 1; // Return with error code 1
 
     // set an interval to rotate images and store the function id
     jBirImgRotator.rotator_id = setInterval(function() {
@@ -245,10 +239,7 @@ const jBir = {
       element.style.backgroundSize = jBirImgRotator.og_background_size_property; // Revert background size property
       element.style.background = jBirImgRotator.og_background_property; // Revert background property
     });
-    this.jBirImgRotators.splice(
-      this.jBirImgRotators.indexOf(jBirImgRotator),
-      1
-    ); // Destroy image rotator
+    this.jBirImgRotators.splice(this.jBirImgRotators.indexOf(jBirImgRotator), 1); // Destroy image rotator
     return 0; // success
   },
   // Function for removing all rotators
@@ -272,15 +263,13 @@ class jBirImgRotator {
     "https://images.unsplash.com/photo-1502691876148-a84978e59af8",
     "https://images.unsplash.com/photo-1463438690606-f6778b8c1d10",
     "https://images.unsplash.com/photo-1538291323976-37dcaafccb12",
-    "https://images.unsplash.com/photo-1524293568345-75d62c3664f7",
     "https://images.unsplash.com/photo-1514525253161-7a46d19cd819",
     "https://images.unsplash.com/photo-1529912626516-e58b23f44f1d",
     "https://images.unsplash.com/photo-1545231097-c046d9dcc2f6",
     "https://images.unsplash.com/photo-1558470598-a5dda9640f68",
     "https://images.unsplash.com/photo-1545231097-cbd796f1d95f",
     "https://images.unsplash.com/photo-1518516278006-4aca92806257",
-    "https://images.unsplash.com/photo-1533669955142-6a73332af4db",
-    "not an actual url so it will fail to preload and will be removed from the image url array"
+    "https://images.unsplash.com/photo-1533669955142-6a73332af4db"
   ];
   interval_ms = 10000; // default interval between image rotations
   transition_ms = 3000; // default transition time
